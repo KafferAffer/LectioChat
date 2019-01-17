f426c4867dfd0093d1907ce0bc466523faf4e521
KafferAffer <46445011+KafferAffer@users.noreply.github.com>
2019-01-17 10:00:59 +0100

    I done did do a lot of faggot buttons

--- popup.js
+++ popup.js
@@ -14,7 +14,7 @@ changeColor.onclick = function(element) {
     let color = element.target.value;
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: 'document.body.style.backgroundColor = "' + color + '";'});
+        tabs[0].id,
+        {code: 'var btn = document.createElement("BUTTON");var t = document.createTextNode("CLICK ME Faggot");btn.appendChild(t);document.body.appendChild(btn);'});
     });
 };
