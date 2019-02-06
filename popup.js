d20388297f3f2bd09b8b0f4cc7e461b38b2457a1
KafferAffer <46445011+KafferAffer@users.noreply.github.com>
2019-02-06 13:47:15 +0100

    reee

--- popup.js
+++ popup.js
@@ -10,11 +10,26 @@ chrome.storage.sync.get('color', function(data) {
   changeColor.setAttribute('value', data.color);
 });
 
+
+
+
+
+var htmlCode1 = 'var box = document.createElement("div");box.setAttribute("id","p2");var xd = document.createTextNode("click me");box.appendChild(xd);document.body.appendChild(box);';
+var htmlCode2 = 'box.style.width = "200px";box.style.height = "200px";box.style.backgroundColor = "blue";';
+var htmlCode3 = 'box.style.position = "fixed";box.style.bottom = "0";box.style.right = "0";';
+var htmlCode4 = 'var btn = document.createElement("BUTTON");box.appendChild(btn);';
+var htmlCode5 = 'var btnt = document.createTextNode("hej");btn.appendChild(btnt);';
+var htmlCode6 = 'box.style.position = "fixed";box.style.bottom = "0px";btn.style.width = "100px";btn.style.height = "50px";btn.style.backgroundColor = "green";';
+
+
+var finalcode = htmlCode1.concat(htmlCode2,htmlCode3,htmlCode4,htmlCode5,htmlCode6);
+
 changeColor.onclick = function(element) {
     let color = element.target.value;
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.executeScript(
         tabs[0].id,
-        {code: 'var btn = document.createElement("h1");var t = document.createTextNode("CLICK ME Faggot");btn.setAttribute("color","red");btn.appendChild(t);document.body.appendChild(btn);'});
+        {code: finalcode});
     });
 };
+
